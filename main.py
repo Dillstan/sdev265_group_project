0b5d2e245ea88d9ab3746cc722edbfd330e45b2f
Kobe Hillman <131305116+kobehillman@users.noreply.github.com>
2025-01-27 02:47:12 -0500

    - Began designing main page
    - Added toolbar on the left side
    
    - Added top uppermost frame to hold current username, settings, button, and picture
    
    - Added icon images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 from CTkMessagebox import CTkMessagebox
+from subprocess import call
+import sqlite3
 
 # ATT KOBE:
 ## RANDOM PASSWORD GEN - STRING OF NUMBERS AND LETTERS "STRONG" PASSWORD"
@@ -16,7 +18,58 @@ from CTkMessagebox import CTkMessagebox
 
 
 ##
+
+
+def cursor_on_hover(button):
+    button.configure(cursor='hand2')
+
+
+def reset_cursor_on_leave(button):
+    button.configure(cursor='none')
+
+
+def fetch_current_user(database):
+   try:
+    users = sqlite3.connect(database)
+    mycursor = users.cursor()
+
+    command = "select * from users where LoggedIn = 1"
+    mycursor.execute(command)
+
+    result = mycursor.fetchone()
+
+    logged_in_user = result[1]
+
+    return logged_in_user
+
+   except:
+       logged_in_user = None
+
+
+
+def exit_button(window):
+    users = sqlite3.connect("appdata.db")
+    mycursor = users.cursor()
+
+    command = "update users set LoggedIn = 0 where LoggedIn = 1"
+    mycursor.execute(command)
+    users.commit()
+    users.close()
+
+    window.destroy()
+
+
+def clear_frame(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
 class Main(ctk.CTk):
+
+    def show_frames(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
     def __init__(self, *args, **kwargs):
         ctk.CTk.__init__(self, *args, **kwargs)
         self.wm_title('Main | Password Manager')
@@ -25,51 +78,115 @@ class Main(ctk.CTk):
         self.resizable(False, False)
         self.wm_iconbitmap('appicon.ico')
         self.eval("tk::PlaceWindow . center")
+        self.protocol("WM_DELETE_WINDOW", lambda: exit_button(self))
+        self.configure(fg_color='#212c56')
 
         # CREATES AND PLACES FRAME FOR PAGES ON THE RIGHT SIDE
-        container = ctk.CTkFrame(self, height=580, width=740, fg_color="red")
-        container.place(x=250, y=10)
+        container = ctk.CTkFrame(self, height=500, width=900, fg_color="#212c56")
+        container.place(x=100, y=100)
 
         self.frames = {}
 
         for F in (HomePage, StoredPasswordsPage, SettingsPage, AIPage):
             frame = F(container, self)
-            frame.configure(height=580, width=740)
+            frame.configure(height=500, width=900)
 
             self.frames[F] = frame
             frame.place(x=0, y=0)
 
         self.show_frames(HomePage)
 
-    def show_frames(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
+        # ----------------- TOOLBAR BASE ----------------------- #
+        toolbar_top = ctk.CTkFrame(self, height=60, width=60, fg_color='#151c36')
+        toolbar_top.place(x=15, y=60)
+
+        toolbar_middle = ctk.CTkFrame(self, height=300, width=60, fg_color='#151c36')
+        toolbar_middle.place(x=15, y=140)
+
+        toolbar_bottom = ctk.CTkFrame(self, height=60, width=60, fg_color='#151c36')
+        toolbar_bottom.place(x=15, y=460)
+
+        separator_line = ctk.CTkFrame(self, height=600, width=2, fg_color="#151c36")
+        separator_line.place(x=90, y=0)
+
+        # ----------------- TOOLBAR ICONS ----------------------- #
+        home_icon = ctk.CTkImage(Image.open('toolbar_icons/home_icon.png'))
+        home_icon._size = 40, 40
+
+        add_icon = ctk.CTkImage(Image.open('toolbar_icons/add_icon.png'))
+        add_icon._size = 40, 40
+
+        toolbar_logo = ctk.CTkImage(Image.open('main_menu_logo.png'))
+        toolbar_logo._size = 50, 50
+
+        # ----------------- TOOLBAR IMAGE HOLDERS & BUTTONS ----------------------- #
+        home_icon_button = ctk.CTkButton(toolbar_middle, height=60, width=60, image=home_icon, fg_color='transparent', text='', hover=False)
+        home_icon_button.place(x=0, y=3)
+
+        add_icon_button = ctk.CTkButton(toolbar_bottom, height=40, width=40, image=add_icon, fg_color='transparent', text='', hover=False)
+        add_icon_button.place(x=2, y=6)
+
+        toolbar_logo_label = ctk.CTkLabel(toolbar_top, text='', image=toolbar_logo)
+        toolbar_logo_label.place(x=5, y=5)
+
+        # ----------------- TOOLBAR MISC FUNCTIONALITY ----------------------- #
+        home_icon_button.bind('<Enter>', lambda event: cursor_on_hover(home_icon_button))
+        home_icon_button.bind('<Leave>', lambda event: reset_cursor_on_leave(home_icon_button))
+
+        add_icon_button.bind('<Enter>', lambda event: cursor_on_hover(add_icon_button))
+        add_icon_button.bind('<Leave>', lambda event: reset_cursor_on_leave(add_icon_button))
+
+        # ----------------- TOP BAR --------------------- #
+
+        top_bar = ctk.CTkFrame(self, height=90, width=900, fg_color='#212c56')
+        top_bar.place(x=99, y=5)
+
+        settings_icon = ctk.CTkImage(Image.open('toolbar_icons/settings_icon.png'))
+        settings_icon._size = 35, 35
+
+        settings_icon_button = ctk.CTkButton(top_bar, image=settings_icon, fg_color='transparent', text='', hover=False)
+        settings_icon_button.place(x=790, y=25)
+
+        settings_icon_button.bind('<Enter>', lambda event: cursor_on_hover(settings_icon_button))
+        settings_icon_button.bind('<Leave>', lambda event: reset_cursor_on_leave(settings_icon_button))
+
+        greeting_label = ctk.CTkLabel(top_bar, text='Hello,', font=("Helvetica", 20, "bold"), width=200)
+        greeting_label.place(x=40, y=3)
+
+        current_user = fetch_current_user('appdata.db')
+
+        current_user_label = ctk.CTkLabel(top_bar, text=current_user, font=("Helvetica", 18, "bold"), width=200)
+        current_user_label.place(x=70, y=30)
+
 
 class HomePage(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        ctk.CTkFrame.__init__(self, parent, fg_color="red")
+        ctk.CTkFrame.__init__(self, parent, fg_color='#212c56')
 
         button = ctk.CTkButton(self, text='Home Page | Click For Stored Passwords', command=lambda: controller.show_frames(StoredPasswordsPage))
         button.place(x=50, y=100)
 
+
 class StoredPasswordsPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        ctk.CTkFrame.__init__(self, parent, fg_color="blue")
+        ctk.CTkFrame.__init__(self, parent, fg_color='#212c56')
 
         button = ctk.CTkButton(self, text='Stored Passwords | Click For Settings', command=lambda: controller.show_frames(SettingsPage))
         button.place(x=50, y=100)
 
+
 class SettingsPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        ctk.CTkFrame.__init__(self, parent, fg_color="black")
+        ctk.CTkFrame.__init__(self, parent, fg_color='#212c56')
         button = ctk.CTkButton(self, text='Settings | Click to to AI Page', command=lambda: controller.show_frames(AIPage))
         button.place(x=50, y=100)
 
+
 class AIPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        ctk.CTkFrame.__init__(self, parent, fg_color="green")
+        ctk.CTkFrame.__init__(self, parent, fg_color='#212c56')
         button = ctk.CTkButton(self, text="AI Page | Click to go back to home page", command=lambda: controller.show_frames(HomePage))
-
+        button.place(x=50, y=100)
 
 
 if __name__ == '__main__':
